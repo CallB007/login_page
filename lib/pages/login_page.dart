73d3027c9789b9062f8eca9c6193244a2da4db25
CallB007 <josephwijaya80@gmail.com>
2023-05-20 12:24:52 +0800

    edit

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,7 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:login_page/component/button.dart';
+import 'package:login_page/component/square_tile.dart';
+import 'package:login_page/component/textfield.dart';
 
 class loginPage extends StatelessWidget {
-  const loginPage({super.key});
+  loginPage({super.key});
+
+  //controller textfield
+  final usernameController = TextEditingController();
+  final passwordController = TextEditingController();
+
+  //signIn Method
+  void signIn() {}
 
   @override
   Widget build(BuildContext context) {
@@ -10,30 +20,121 @@ class loginPage extends StatelessWidget {
       body: SafeArea(
         child: Center(
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Icon(
-                Icons.login,
-                size: 100,
-              ),
-
               SizedBox(
                 height: 50,
               ),
-              //logo
+
+              //logo icon Lock
+              Icon(
+                Icons.lock,
+                size: 100,
+              ),
 
               //Selamat Datang di Aplikasi kami
+              SizedBox(
+                height: 50,
+              ),
+              Text(
+                'Silahkan Masukan akun terlebih dahulu',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 16,
+                ),
+              ),
+              SizedBox(
+                height: 50,
+              ),
 
               // username
+              MyTextField(
+                controller: usernameController,
+                hintText: ('Username'),
+                obsecureText: false,
+              ),
+              SizedBox(
+                height: 10,
+              ),
 
               //password
+              MyTextField(
+                controller: passwordController,
+                hintText: ('Password'),
+                obsecureText: true,
+              ),
+              SizedBox(
+                height: 20,
+              ),
 
               //forgot password
+              Padding(
+                padding: const EdgeInsets.only(right: 35),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Text(
+                      'Lupa Password ? ',
+                      style: TextStyle(color: Colors.black),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
 
               //button
+              MyButton(
+                onTap: signIn,
+              ),
+              SizedBox(
+                height: 50,
+              ),
 
               //continue with google
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 45),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: Divider(thickness: 0.5, color: Colors.black),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 15),
+                      child: Text('Atau lanjut dengan'),
+                    ),
+                    Expanded(
+                      child: Divider(thickness: 0.5, color: Colors.black),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 30,
+              ),
+
+              //logo google
+              squareTile(imagePath: 'icons/google.png'),
+              SizedBox(
+                height: 30,
+              ),
 
               //register now
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text('Belum punya akun ?'),
+                  SizedBox(
+                    width: 5,
+                  ),
+                  Text(
+                    'Daftar Sekarang',
+                    style: TextStyle(
+                        color: Colors.blue, fontWeight: FontWeight.bold),
+                  ),
+                ],
+              ),
             ],
           ),
         ),
